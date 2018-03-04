9ea4644affc7b7605377a141d30feb4c6144453c
turkishlightning <37043333+turkishlightning@users.noreply.github.com>
2018-03-04 09:28:55 -0500

    Add files via upload
    Actual initial working code

---
+++ project0.c
@@ -0,0 +1,163 @@
+//*****************************************************************************
+//
+// project0.c - Senior Design Software, ECE 49022 -- Purdue University, Smart Dog Door, Holden Yildirim
+//
+//
+//*****************************************************************************
+
+//Libraries
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "inc/hw_types.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_GPIO.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/gpio.h"
+#include "driverlib/pwm.h"
+#include "driverlib/timer.h"
+#include "driverlib/pin_map.h"
+
+//*****************************************************************************
+// Variable Declarations
+
+int drive = 1;    //flag to show that sensors and RFID are valid     <--------------------------change
+int fwd1 = 0;   //flag to show that lock has passed first forward switch (slow down)
+int fwd2 = 0;   //flag to show that lock has past second forward switch (kill-switch)
+int back1 = 0;  //flag to show that lock has passed first backward switch (slow down)
+int back2 = 0;  //flag to show that lock has passed second backward switch (kill-switch)
+
+
+
+//*****************************************************************************
+// Function Declarations
+
+void PortEIntHandler(void); //Port E interrupt handler
+
+
+//*****************************************************************************
+//
+// The error routine that is called if the driver library encounters an error.
+//
+//*****************************************************************************
+#ifdef DEBUG
+void
+__error__(char *pcFilename, uint32_t ui32Line)
+{
+}
+#endif
+
+//*****************************************************************************
+//
+// Main 'C' Language entry point.  Toggle an LED using TivaWare.
+//
+//*****************************************************************************
+int
+main(void)
+{
+    //
+    // Setup the system clock to run at 50 Mhz from PLL with crystal reference
+    //
+    SysCtlClockSet(SYSCTL_SYSDIV_4|SYSCTL_USE_PLL|SYSCTL_XTAL_16MHZ|
+                    SYSCTL_OSC_MAIN);
+
+    //PWM Initializations
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOF))
+    {
+    }
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_PWM1);
+
+    SysCtlPWMClockSet(SYSCTL_PWMDIV_2);  //50MHZ clock for PWM
+
+    HWREG(GPIO_PORTF_BASE + GPIO_O_LOCK) = GPIO_LOCK_KEY;
+    HWREG(GPIO_PORTF_BASE + GPIO_O_CR) |= 0x01;
+
+    GPIOPinConfigure(GPIO_PF0_M1PWM4);
+    GPIOPinTypePWM(GPIO_PORTF_BASE, GPIO_PIN_0);
+
+    //PWM Configurations
+    PWMGenConfigure(PWM1_BASE, PWM_GEN_2, PWM_GEN_MODE_DOWN | PWM_GEN_MODE_NO_SYNC);
+    PWMGenPeriodSet(PWM1_BASE, PWM_GEN_2, 400);                                             //400 clock ticks = 125 khz
+    //PWMPulseWidthSet(PWM1_BASE, PWM_OUT_4, 200);                                          //Duty cycle
+    PWMGenEnable(PWM1_BASE, PWM_GEN_2);                                                     //PWM enable
+  //  PWMOutputState(PWM1_BASE, PWM_OUT_4_BIT, true);// temporary
+
+    //Switch Interrupt Initializations
+    //Back1 switch
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOE);
+    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOE))
+    {
+    }
+    GPIOPinTypeGPIOInput(GPIO_PORTE_BASE, GPIO_PIN_0);
+    GPIOPadConfigSet(GPIO_PORTE_BASE,GPIO_PIN_0,GPIO_STRENGTH_2MA,GPIO_PIN_TYPE_STD_WPU);   //Configure weak pull-up
+    GPIOIntTypeSet(GPIO_PORTE_BASE,GPIO_PIN_0,GPIO_FALLING_EDGE);                           //Trigger interrupt on falling edge
+    GPIOIntRegister(GPIO_PORTE_BASE,PortEIntHandler);                                       //Set handler function
+    GPIOIntEnable(GPIO_PORTE_BASE, GPIO_INT_PIN_0);                                         //Enable interrupt
+    //Back2 Switch
+    GPIOPinTypeGPIOInput(GPIO_PORTE_BASE, GPIO_PIN_1);
+    GPIOPadConfigSet(GPIO_PORTE_BASE,GPIO_PIN_1,GPIO_STRENGTH_2MA,GPIO_PIN_TYPE_STD_WPU);   //Configure weak pull-up
+    GPIOIntTypeSet(GPIO_PORTE_BASE,GPIO_PIN_1,GPIO_FALLING_EDGE);                           //Trigger interrupt on falling edge
+    GPIOIntRegister(GPIO_PORTE_BASE,PortEIntHandler);                                       //Set handler function
+    GPIOIntEnable(GPIO_PORTE_BASE, GPIO_INT_PIN_1);                                         //Enable interrupt
+
+    // Loop Forever
+
+    while(1)
+    {
+        if(drive == 1)
+        {
+            if((back1 == 0) && (back2 == 0))
+            {
+                PWMOutputState(PWM1_BASE, PWM_OUT_4_BIT, true);         //enable unlock PWM
+                PWMPulseWidthSet(PWM1_BASE, PWM_OUT_4, 300);
+            }
+            else if(back1 == 1)
+            {
+                PWMOutputState(PWM1_BASE, PWM_OUT_4_BIT, true);
+                PWMPulseWidthSet(PWM1_BASE, PWM_OUT_4, 200);        //Reduce duty cycle
+            }
+            else if(back2 == 1)
+            {
+                PWMOutputState(PWM1_BASE, PWM_OUT_4_BIT, false);    //Shut off PWM
+                //need to delay 5 seconds in unlocked state
+            }
+        }
+        /*else
+        {   //reset flags
+            fwd1 = 0;
+            fwd2 = 0;
+            back1 = 0;
+            back2 = 0;
+        }*/
+    }
+}
+
+//*****************************************************************************
+//
+// Port F Interrupt Handler
+//
+//*****************************************************************************
+
+void PortEIntHandler(void)
+{
+    uint32_t    status = 0;                             //Status variable to determine which pin triggered interrupt
+    status = GPIOIntStatus(GPIO_PORTE_BASE,true);       //More pins will be added for interrupts
+    GPIOIntClear(GPIO_PORTE_BASE,status);
+    if( (status & GPIO_INT_PIN_0) == GPIO_INT_PIN_0)
+    {
+        //Then there was a pin0 interrupt, reduce speed
+        back1 = 1;
+        back2 = 0;
+    }
+    else if( (status & GPIO_INT_PIN_1) == GPIO_INT_PIN_1)
+    {
+        //Then there was a pin1 interrupt, stop motor
+        back2 = 1;
+        back1 = 0;
+    }
+
+}
+
+
+
