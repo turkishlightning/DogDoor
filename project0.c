2dbc58a4a2311a9dd5636eddea515e70d8ef3c58
turkishlightning <37043333+turkishlightning@users.noreply.github.com>
2018-03-06 12:29:19 -0500

    Add files via upload
    UART working. BUG: requires 2 UART prompt sequences to restart prototype

--- project0.c
+++ project0.c
@@ -7,8 +7,11 @@
 
 //Libraries
 
+#include <tm4c123gh6pm.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
 #include "inc/hw_types.h"
 #include "inc/hw_memmap.h"
 #include "inc/hw_GPIO.h"
@@ -18,25 +21,33 @@
 #include "driverlib/pwm.h"
 #include "driverlib/timer.h"
 #include "driverlib/pin_map.h"
+#include "driverlib/rom.h"
+#include "driverlib/rom_map.h"
 #include "driverlib/uart.h"
-#include "utils/uartstdio.h"
+#include "uartstdio.h"
 
 //*****************************************************************************
 // Variable Declarations
 
-int drive = 1;    //flag to show that sensors and RFID are valid     <--------------------------change
-int fwd1 = 0;   //flag to show that lock has passed first forward switch (slow down)
-int fwd2 = 0;   //flag to show that lock has past second forward switch (kill-switch)
-int back1 = 0;  //flag to show that lock has passed first backward switch (slow down)
-int back2 = 0;  //flag to show that lock has passed second backward switch (kill-switch)
-int i = 0;      //delay loop counter
+int drive = 0;      //flag to show that sensors and RFID are valid
+int fwd1 = 0;       //flag to show that lock has passed first forward switch (slow down)
+int fwd2 = 0;       //flag to show that lock has past second forward switch (kill-switch)
+int back1 = 0;      //flag to show that lock has passed first backward switch (slow down)
+int back2 = 0;      //flag to show that lock has passed second backward switch (kill-switch)
+int i = 0;          //delay loop counter
+char sensors = 'n'; // y or n indicating sensors in range
+char rfid = 'n';    // y or n indicating sensors in range
 
 
 
 //*****************************************************************************
 // Function Declarations
 
-void PortEIntHandler(void); //Port E interrupt handler
+void PortEIntHandler(void);         //Port E interrupt handler
+char readChar(void);                //UART character reader
+void printChar(char c);             //UART character print
+void printString(char * string);    //UART string print
+char* readString(char delimiter);   //UART string reader
 
 
 //*****************************************************************************
@@ -84,9 +95,7 @@ main(void)
     GPIOPinTypePWM(GPIO_PORTF_BASE, GPIO_PIN_0 | GPIO_PIN_1);
     PWMGenConfigure(PWM1_BASE, PWM_GEN_2, PWM_GEN_MODE_DOWN | PWM_GEN_MODE_NO_SYNC);
     PWMGenPeriodSet(PWM1_BASE, PWM_GEN_2, 400);                                             //400 clock ticks = 125 khz
-    //PWMPulseWidthSet(PWM1_BASE, PWM_OUT_4, 200);                                          //Duty cycle
     PWMGenEnable(PWM1_BASE, PWM_GEN_2);                                                     //PWM enable
-  //  PWMOutputState(PWM1_BASE, PWM_OUT_4_BIT, true);// temporary
 
     //Switch Interrupt Initializations
     //Back1 switch
@@ -118,6 +127,17 @@ main(void)
     GPIOIntRegister(GPIO_PORTE_BASE,PortEIntHandler);                                       //Set handler function
     GPIOIntEnable(GPIO_PORTE_BASE, GPIO_INT_PIN_3);                                         //Enable interrupt
 
+    //UART Initializations
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);                                            //Enable GPIO Port A
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);                                            //Enable UART Module 0
+    GPIOPinConfigure(GPIO_PA0_U0RX);                                                        //MUX UART to Port A pins 0 and 1
+    GPIOPinConfigure(GPIO_PA1_U0TX);
+    GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+    UARTClockSourceSet(UART0_BASE, UART_CLOCK_SYSTEM);                                       //Set UART clock
+    UARTStdioConfig(0, 9600, SysCtlClockGet());
+
+    UARTprintf("Welcome to the Dog Door Motor Control Prototype\n");
+
     // Loop Forever
 
     while(1)
@@ -128,8 +148,11 @@ main(void)
             {
                 PWMPulseWidthSet(PWM1_BASE, PWM_OUT_4, 280);
                 PWMOutputState(PWM1_BASE, PWM_OUT_4_BIT, true);     //enable unlock PWM
+                GPIOIntEnable(GPIO_PORTE_BASE, GPIO_INT_PIN_0);     //Enable SW1 and SW2 interrupts
+                GPIOIntEnable(GPIO_PORTE_BASE, GPIO_INT_PIN_1);
                 GPIOIntDisable(GPIO_PORTE_BASE, GPIO_INT_PIN_2);    //Disable locking interrupts SW3 and SW4
                 GPIOIntDisable(GPIO_PORTE_BASE, GPIO_INT_PIN_3);
+                UARTprintf("Unlocking...\n");
             }
             else if(back1 == 1)
             {
@@ -139,13 +162,13 @@ main(void)
             else if(back2 == 1)
             {
                 PWMOutputState(PWM1_BASE, PWM_OUT_4_BIT, false);    //Shut off PWM1
-               // SysCtlDelay(3*SysCtlClockGet()/3);                  //Delay 3s, need to do only once <-----------------------------------------------------
                 PWMPulseWidthSet(PWM1_BASE, PWM_OUT_5, 280);        //Set PWM2 to full speed
                 PWMOutputState(PWM1_BASE, PWM_OUT_5_BIT, true);     //Turn on PWM2 (lock)
                 GPIOIntDisable(GPIO_PORTE_BASE, GPIO_INT_PIN_0);
                 GPIOIntDisable(GPIO_PORTE_BASE, GPIO_INT_PIN_1);    //Disable interrupts on SW1 and SW2
-                GPIOIntEnable(GPIO_PORTE_BASE, GPIO_INT_PIN_2);
+                GPIOIntEnable(GPIO_PORTE_BASE, GPIO_INT_PIN_2);     //Enable interrupts for SW3 and SW4
                 GPIOIntEnable(GPIO_PORTE_BASE, GPIO_INT_PIN_3);
+                UARTprintf("Locking...\n");
 
             }
             else if(fwd1 == 1)
@@ -157,21 +180,46 @@ main(void)
             {
                 PWMOutputState(PWM1_BASE, PWM_OUT_5_BIT, false);    //Stop motor
                 drive = 0;
+                /*GPIOIntEnable(GPIO_PORTE_BASE, GPIO_INT_PIN_0);     //Enable SW1 and SW2 interrupts
+                GPIOIntEnable(GPIO_PORTE_BASE, GPIO_INT_PIN_1);
+                GPIOIntDisable(GPIO_PORTE_BASE, GPIO_INT_PIN_2);    //Disable locking interrupts SW3 and SW4
+                GPIOIntDisable(GPIO_PORTE_BASE, GPIO_INT_PIN_3);*/
             }
         }
-        /*else
-        {   //reset flags
+        else
+        {
             fwd1 = 0;
             fwd2 = 0;
             back1 = 0;
             back2 = 0;
-        }*/
+            UARTprintf("Are all sensors within range?(y/n)\n\r");
+            sensors = readChar();
+            if(sensors == 'y')
+            {
+                UARTprintf("Is RFID tag in range?(y/n)\n\r");
+                rfid = readChar();
+                if(rfid == 'y')
+                {
+                    drive = 1;
+                }
+                else
+                {
+                    UARTprintf("Invalid Input\n\r");
+                    drive = 0;
+                }
+            }
+            else
+            {
+                UARTprintf("Invalid Input\n\r");
+                drive = 0;
+            }
+        }
     }
 }
 
 //*****************************************************************************
 //
-// Port F Interrupt Handler
+// Port E Interrupt Handler
 //
 //*****************************************************************************
 
@@ -217,5 +265,80 @@ void PortEIntHandler(void)
 
 }
 
+//*****************************************************************************
+//
+// UART Print Character Function, credit:AllAboutEE
+//
+//*****************************************************************************
+
+void printChar(char c)
+{
+    while((UART0_FR_R & (1<<5)) != 0);
+    UART0_DR_R = c;
+}
+
+//*****************************************************************************
+//
+// UART Print String Function, credit:AllAboutEE
+//
+//*****************************************************************************
+
+void printString(char * string)
+{
+  while(*string)
+  {
+    printChar(*(string++));
+  }
+}
+
+//*****************************************************************************
+//
+// UART Read Character Function, credit:AllAboutEE
+//
+//*****************************************************************************
 
 
+char readChar(void)
+{
+    char c;
+    while((UART0_FR_R & (1<<4)) != 0);
+    c = UART0_DR_R;
+    return c;
+}
+
+//*****************************************************************************
+//
+// UART Read String Function, credit:AllAboutEE
+//
+//*****************************************************************************
+
+char* readString(char delimiter)
+{
+
+  int stringSize = 0;
+  char* string = (char*)calloc(10,sizeof(char));
+  char c = readChar();
+  printChar(c);
+
+  while(c!=delimiter)
+  {
+
+    *(string+stringSize) = c; // put the new character at the end of the array
+    stringSize++;
+
+    if((stringSize%10) == 0) // string length has reached multiple of 10
+    {
+      string = (char*)realloc(string,(stringSize+10)*sizeof(char)); // adjust string size by increasing size by another 10
+    }
+
+    c = readChar();
+    printChar(c); // display the character the user typed
+  }
+
+  if(stringSize == 0)
+  {
+
+    return '\0'; // null car
+  }
+  return string;
+}
